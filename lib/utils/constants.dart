d73528b43781fcb3c400ee00be18fd4432f6266f
prathap021 <selflearner021@gmail.com>
2023-11-19 01:35:00 +0530

    toast bottom bar implementing

--- lib/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiServices {
-  static const String endpoint = 'https://ta.wiktionary.org/w/api.php';
-}
---
+++ lib/utils/networkConstants.dart
@@ -0,0 +1,3 @@
+class ApiServices {
+  static const String baseUrl = 'https://ta.wiktionary.org/w/api.php';
+}
